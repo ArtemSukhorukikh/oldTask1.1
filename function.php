d4347573fcb49184902ee7c78e8392d387365afc
ArtemSukhorukikh <des1337481@gmail.com>
2019-07-27 00:01:03 +0300

    task-function-number

--- function.php
+++ function.php
@@ -1,11 +1,13 @@
 <?php
-function declOfNum($number,$one,$two,$tree){
+function declOfNum($number,$one,$two,$tree,$shownumber){
     $resultString = "";
+    if ($shownumber == true)
+    	$resultString = $resultString.$number;
     if($number %10 == 0 || ($number % 10 >= 5 && $number % 10 <= 9))
-        $resultString = "$number $tree";
+        $resultString = $resultString.$tree;
     elseif ($number %10 == 1)
-        $resultString = "$number $one";
+        $resultString = $resultString.$one;
     else
-        $resultString = "$number $two";
+        $resultString = $resultString.$two;
     return $resultString;
 }
