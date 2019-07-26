6994fceacebc84ed361096f27195c53f55980d9d
ArtemSukhorukikh <des1337481@gmail.com>
2019-07-26 23:56:41 +0300

    task-function-array

--- function.php
+++ function.php
@@ -1,11 +1,12 @@
 <?php
-function declOfNum($number,$one,$two,$tree){
+function declOfNum($number,array $fields){
     $resultString = "";
+    $resultString = $resultString.$number;
     if($number %10 == 0 || ($number % 10 >= 5 && $number % 10 <= 9))
-        $resultString = "$number $tree";
+        $resultString = $resultString.$fields[2];
     elseif ($number %10 == 1)
-        $resultString = "$number $one";
+        $resultString = $resultString.$fields[0];
     else
-        $resultString = "$number $two";
+        $resultString = $resultString.$fields[2];
     return $resultString;
 }
